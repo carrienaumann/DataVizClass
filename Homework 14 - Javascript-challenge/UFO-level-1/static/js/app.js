ef19e74631b18f61810ad29c4306ff5e00722c1a
Carrie Naumann <carrienaumann@gmail.com>
2020-02-12 16:47:34 -0500

    add hw 14

---
+++ Homework 14 - Javascript-challenge/UFO-level-1/static/js/app.js
@@ -0,0 +1,48 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+// from data.js
+
+var tbody = d3.select("tbody");
+
+console.log(tableData);
+
+function createTable(data) {
+  tbody.html("");
+
+  data.forEach((sighting) => {
+    
+      var row = tbody.append("tr");
+      Object.entries(sighting).forEach(function([key, value]) {
+        console.log(key, value);
+            
+        var cell = row.append("td");
+        cell.text(value);
+    });
+  });
+}
+
+function buttonClick() {
+ 
+  var inputElement = d3.select("#datetime");
+
+  var inputValue = inputElement.property("value");
+  let filteredData = tableData;
+
+  console.log(inputValue);
+  console.log(tableData);
+  
+  if (inputValue) {
+    filteredData = tableData.filter(sightings => sightings.datetime === inputValue);
+}
+  
+console.log(filteredData);
+
+createTable(filteredData);
+}
+
+var button = d3.select("#filter-btn");
+button.on("click", buttonClick);
+
+createTable(tableData);
